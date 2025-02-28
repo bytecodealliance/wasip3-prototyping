7eb0a941042b1eb76a53d1abae1c4ba6bdd952a6
Roman Volosatovs <rvolosatovs@riseup.net>
2025-02-28 15:53:16 +0100

    feat: implement `wasi:cli`
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- crates/wasi/src/p3/cli/mod.rs
+++ crates/wasi/src/p3/cli/mod.rs
@@ -1,5 +1,10 @@
 mod host;
 
+use core::fmt;
+
+use tokio::io::{
+    empty, stderr, stdin, stdout, AsyncRead, AsyncWrite, Empty, Stderr, Stdin, Stdout,
+};
 use wasmtime::component::{Linker, ResourceTable};
 
 use crate::p3::ResourceView;
@@ -33,6 +38,9 @@ pub struct WasiCliCtx {
     pub environment: Vec<(String, String)>,
     pub arguments: Vec<String>,
     pub initial_cwd: Option<String>,
+    pub stdin: Box<dyn InputStream + Send>,
+    pub stdout: Box<dyn OutputStream + Send>,
+    pub stderr: Box<dyn OutputStream + Send>,
 }
 
 impl Default for WasiCliCtx {
@@ -41,6 +49,9 @@ impl Default for WasiCliCtx {
             environment: Vec::default(),
             arguments: Vec::default(),
             initial_cwd: None,
+            stdin: Box::new(empty()),
+            stdout: Box::new(empty()),
+            stderr: Box::new(empty()),
         }
     }
 }
@@ -136,4 +147,143 @@ where
     F: Fn(&mut T) -> WasiCliImpl<&mut T>,
 {
     val
+}
+
+/// An error returned from the `proc_exit` host syscall.
+///
+/// Embedders can test if an error returned from wasm is this error, in which
+/// case it may signal a non-fatal trap.
+#[derive(Debug)]
+pub struct I32Exit(pub i32);
+
+impl fmt::Display for I32Exit {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Exited with i32 exit status {}", self.0)
+    }
+}
+
+impl std::error::Error for I32Exit {}
+
+pub struct TerminalInput;
+pub struct TerminalOutput;
+
+pub trait IsTerminal {
+    /// Returns whether this stream is backed by a TTY.
+    fn is_terminal(&self) -> bool;
+}
+
+impl IsTerminal for Empty {
+    fn is_terminal(&self) -> bool {
+        false
+    }
+}
+
+pub trait InputStream: IsTerminal {
+    fn reader(&self) -> Box<dyn AsyncRead + Send + Sync + Unpin>;
+}
+
+pub trait OutputStream: IsTerminal {
+    fn writer(&self) -> Box<dyn AsyncWrite + Send + Sync + Unpin>;
+}
+
+impl InputStream for Empty {
+    fn reader(&self) -> Box<dyn AsyncRead + Send + Sync + Unpin> {
+        Box::new(empty())
+    }
+}
+
+impl OutputStream for Empty {
+    fn writer(&self) -> Box<dyn AsyncWrite + Send + Sync + Unpin> {
+        Box::new(empty())
+    }
+}
+
+impl IsTerminal for std::io::Empty {
+    fn is_terminal(&self) -> bool {
+        false
+    }
+}
+
+impl InputStream for std::io::Empty {
+    fn reader(&self) -> Box<dyn AsyncRead + Send + Sync + Unpin> {
+        Box::new(empty())
+    }
+}
+
+impl OutputStream for std::io::Empty {
+    fn writer(&self) -> Box<dyn AsyncWrite + Send + Sync + Unpin> {
+        Box::new(empty())
+    }
+}
+
+impl IsTerminal for Stdin {
+    fn is_terminal(&self) -> bool {
+        std::io::stdin().is_terminal()
+    }
+}
+
+impl InputStream for Stdin {
+    fn reader(&self) -> Box<dyn AsyncRead + Send + Sync + Unpin> {
+        Box::new(stdin())
+    }
+}
+
+impl IsTerminal for std::io::Stdin {
+    fn is_terminal(&self) -> bool {
+        std::io::IsTerminal::is_terminal(self)
+    }
+}
+
+impl InputStream for std::io::Stdin {
+    fn reader(&self) -> Box<dyn AsyncRead + Send + Sync + Unpin> {
+        Box::new(stdin())
+    }
+}
+
+impl IsTerminal for Stdout {
+    fn is_terminal(&self) -> bool {
+        std::io::stdout().is_terminal()
+    }
+}
+
+impl OutputStream for Stdout {
+    fn writer(&self) -> Box<dyn AsyncWrite + Send + Sync + Unpin> {
+        Box::new(stdout())
+    }
+}
+
+impl IsTerminal for std::io::Stdout {
+    fn is_terminal(&self) -> bool {
+        std::io::IsTerminal::is_terminal(self)
+    }
+}
+
+impl OutputStream for std::io::Stdout {
+    fn writer(&self) -> Box<dyn AsyncWrite + Send + Sync + Unpin> {
+        Box::new(stdout())
+    }
+}
+
+impl IsTerminal for Stderr {
+    fn is_terminal(&self) -> bool {
+        std::io::stderr().is_terminal()
+    }
+}
+
+impl OutputStream for Stderr {
+    fn writer(&self) -> Box<dyn AsyncWrite + Send + Sync + Unpin> {
+        Box::new(stderr())
+    }
+}
+
+impl IsTerminal for std::io::Stderr {
+    fn is_terminal(&self) -> bool {
+        std::io::IsTerminal::is_terminal(self)
+    }
+}
+
+impl OutputStream for std::io::Stderr {
+    fn writer(&self) -> Box<dyn AsyncWrite + Send + Sync + Unpin> {
+        Box::new(stderr())
+    }
 }
