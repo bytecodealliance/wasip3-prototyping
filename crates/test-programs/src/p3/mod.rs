4db7da395c1fb79e0daf0a226d76c38dc3abddbb
Roman Volosatovs <rvolosatovs@riseup.net>
2025-03-03 18:20:22 +0100

    feat(p3): implement `wasi:http`
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- crates/test-programs/src/p3/mod.rs
+++ crates/test-programs/src/p3/mod.rs
@@ -1,8 +1,19 @@
+pub mod http;
 pub mod sockets;
 
 wit_bindgen::generate!({
-    path: "../wasi/src/p3/wit",
-    world: "wasi:cli/command",
+    inline: "
+        package wasmtime:test;
+
+        world testp3 {
+            include wasi:cli/imports@0.3.0;
+            include wasi:http/imports@0.3.0-draft;
+
+            export wasi:cli/run@0.3.0;
+        }
+    ",
+    path: "../wasi-http/src/p3/wit",
+    world: "wasmtime:test/testp3",
     default_bindings_module: "test_programs::p3",
     pub_export_macro: true,
     async: {
@@ -10,6 +21,7 @@ wit_bindgen::generate!({
              "wasi:clocks/monotonic-clock@0.3.0#wait-for",
              "wasi:clocks/monotonic-clock@0.3.0#wait-until",
              "wasi:filesystem/types@0.3.0#[method]descriptor.write-via-stream",
+             "wasi:http/handler@0.3.0-draft#handle",
              "wasi:sockets/ip-name-lookup@0.3.0#resolve-addresses",
              "wasi:sockets/types@0.3.0#[method]tcp-socket.connect",
              "wasi:sockets/types@0.3.0#[method]tcp-socket.send",
@@ -22,3 +34,37 @@ wit_bindgen::generate!({
     },
     generate_all
 });
+
+pub mod proxy {
+    wit_bindgen::generate!({
+        inline: "
+            package wasmtime:test;
+
+            world proxyp3 {
+                include wasi:http/proxy@0.3.0-draft;
+            }
+        ",
+        path: "../wasi-http/src/p3/wit",
+        world: "wasmtime:test/proxyp3",
+        default_bindings_module: "test_programs::p3::proxy",
+        pub_export_macro: true,
+        async: {
+             imports: [
+                 "wasi:http/handler@0.3.0-draft#handle",
+             ],
+             exports: [
+                 "wasi:http/handler@0.3.0-draft#handle",
+             ],
+        },
+        with: {
+            "wasi:http/handler@0.3.0-draft": generate,
+            "wasi:http/types@0.3.0-draft": crate::p3::wasi::http::types,
+            "wasi:random/random@0.3.0": crate::p3::wasi::random::random,
+            "wasi:cli/stdout@0.3.0": crate::p3::wasi::cli::stdout,
+            "wasi:cli/stderr@0.3.0": crate::p3::wasi::cli::stderr,
+            "wasi:cli/stdin@0.3.0": crate::p3::wasi::cli::stdin,
+            "wasi:clocks/monotonic-clock@0.3.0": crate::p3::wasi::clocks::monotonic_clock,
+            "wasi:clocks/wall-clock@0.3.0": crate::p3::wasi::clocks::wall_clock,
+        },
+    });
+}
---
+++ crates/wasi-http/src/p3/request.rs
@@ -0,0 +1,64 @@
+use core::time::Duration;
+
+use std::sync::Arc;
+
+use http::uri::{Authority, PathAndQuery, Scheme};
+use http::{HeaderMap, Method};
+use wasmtime::component::AbortOnDropHandle;
+use wasmtime_wasi::p3::WithChildren;
+
+use crate::p3::Body;
+
+/// The concrete type behind a `wasi:http/types/request-options` resource.
+#[derive(Clone, Debug, Default)]
+pub struct RequestOptions {
+    /// How long to wait for a connection to be established.
+    pub connect_timeout: Option<Duration>,
+    /// How long to wait for the first byte of the response body.
+    pub first_byte_timeout: Option<Duration>,
+    /// How long to wait between frames of the response body.
+    pub between_bytes_timeout: Option<Duration>,
+}
+/// The concrete type behind a `wasi:http/types/request` resource.
+pub struct Request {
+    /// The method of the request.
+    pub method: Method,
+    /// The scheme of the request.
+    pub scheme: Option<Scheme>,
+    /// The authority of the request.
+    pub authority: Option<Authority>,
+    /// The path and query of the request.
+    pub path_with_query: Option<PathAndQuery>,
+    /// The request headers.
+    pub headers: WithChildren<HeaderMap>,
+    /// Request options.
+    pub options: Option<WithChildren<RequestOptions>>,
+    /// The request body.
+    pub(crate) body: Arc<std::sync::Mutex<Body>>,
+    /// Body stream task handle
+    pub(crate) task: Option<AbortOnDropHandle>,
+}
+
+impl Request {
+    /// Construct a new [Request]
+    pub fn new(
+        method: Method,
+        scheme: Option<Scheme>,
+        authority: Option<Authority>,
+        path_with_query: Option<PathAndQuery>,
+        headers: HeaderMap,
+        body: impl Into<Body>,
+        options: Option<RequestOptions>,
+    ) -> Self {
+        Self {
+            method,
+            scheme,
+            authority,
+            path_with_query,
+            headers: WithChildren::new(headers),
+            body: Arc::new(std::sync::Mutex::new(body.into())),
+            options: options.map(WithChildren::new),
+            task: None,
+        }
+    }
+}
