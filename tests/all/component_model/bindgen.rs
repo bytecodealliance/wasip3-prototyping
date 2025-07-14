a6e4f7d1e3ce6be8037bc0f0d04c868b039aae91
Alex Crichton <alex@alexcrichton.com>
2025-07-14 11:00:04 -0700

    Resolve conflicts with a test

--- tests/all/component_model/bindgen.rs
+++ tests/all/component_model/bindgen.rs
@@ -12,7 +12,7 @@ mod results;
 
 mod no_imports {
     use super::*;
-    // use std::rc::Rc;
+    use std::rc::Rc;
 
     wasmtime::component::bindgen!({
         inline: "
@@ -55,92 +55,11 @@ mod no_imports {
         no_imports.call_bar(&mut store)?;
         no_imports.foo().call_foo(&mut store)?;
 
-        // TODO: re-enable this test once bindings generation is updated
-        // let linker = Linker::new(&engine);
-        // let mut non_send_store = Store::new(&engine, Rc::new(()));
-        // let no_imports = NoImports::instantiate(&mut non_send_store, &component, &linker)?;
-        // no_imports.call_bar(&mut non_send_store)?;
-        // no_imports.foo().call_foo(&mut non_send_store)?;
-        Ok(())
-    }
-}
-
-mod no_imports_concurrent {
-    use super::*;
-    use futures::{
-        FutureExt,
-        stream::{FuturesUnordered, TryStreamExt},
-    };
-
-    wasmtime::component::bindgen!({
-        inline: "
-            package foo:foo;
-
-            world no-imports {
-                export foo: interface {
-                    foo: func();
-                }
-
-                export bar: func();
-            }
-        ",
-        async: true,
-        concurrent_exports: true,
-    });
-
-    #[tokio::test]
-    async fn run() -> Result<()> {
-        let mut config = Config::new();
-        config.wasm_component_model_async(true);
-        config.async_support(true);
-        let engine = &Engine::new(&config)?;
-
-        let component = Component::new(
-            &engine,
-            r#"
-                (component
-                    (core module $m
-                        (import "" "task.return" (func $task-return))
-                        (func (export "bar") (result i32)
-                            call $task-return
-                            i32.const 0
-                        )
-                        (func (export "callback") (param i32 i32 i32) (result i32) unreachable)
-                    )
-                    (core func $task-return (canon task.return))
-                    (core instance $i (instantiate $m
-                        (with "" (instance (export "task.return" (func $task-return))))
-                    ))
-
-                    (func $f (export "bar")
-                        (canon lift (core func $i "bar") async (callback (func $i "callback")))
-                    )
-
-                    (instance $i (export "foo" (func $f)))
-                    (export "foo" (instance $i))
-                )
-            "#,
-        )?;
-
         let linker = Linker::new(&engine);
-        let mut store = Store::new(&engine, ());
-        let instance = linker.instantiate_async(&mut store, &component).await?;
-        let no_imports = NoImports::new(&mut store, &instance)?;
-        let mut futures = FuturesUnordered::new();
-        futures.push(no_imports.call_bar(&mut store).boxed());
-        futures.push(no_imports.foo().call_foo(&mut store).boxed());
-        assert!(
-            instance
-                .run(&mut store, futures.try_next())
-                .await??
-                .is_some()
-        );
-        assert!(
-            instance
-                .run(&mut store, futures.try_next())
-                .await??
-                .is_some()
-        );
+        let mut non_send_store = Store::new(&engine, Rc::new(()));
+        let no_imports = NoImports::instantiate(&mut non_send_store, &component, &linker)?;
+        no_imports.call_bar(&mut non_send_store)?;
+        no_imports.foo().call_foo(&mut non_send_store)?;
         Ok(())
     }
 }
